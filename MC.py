dce4af112802d1cefaf6b8bd82553cab407dd798
Bryson Li <brysonli@gmail.com>
2017-12-21 14:35:46 -0800

    Update MC.py
    fix formatting

--- MC.py
+++ MC.py
@@ -1,6 +1,6 @@
 # MC Practice
 # 12/21/2017
-# A simple way of keeping track of your MC answers (or answers to anhy set of questions).
+# A simple way of keeping track of your MC answers (or answers to any set of questions).
 # stores your answers in a file you specify
 
 print "Hello, welcome to MC pratice."
@@ -19,11 +19,10 @@ print "   ENTER \"quit\" to quit"
 print "-----------------------------"
 
 questionNum = 1
-try:
-  while task != "quit":
-    task = raw_input(str(questionNum) + ". ")
-    if task != "quit":
-        f.write(str(questionNum) + ". " + task + "\n")
-        questionNum+= 1
+while task != "quit":
+	task = raw_input(str(questionNum) + ". ")
+	if task != "quit":
+	    f.write(str(questionNum) + ". " + task + "\n")
+	    questionNum+= 1
 
-print "#End of session"/
+print "#End of session"
